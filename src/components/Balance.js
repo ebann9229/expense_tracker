7675f983d822b73c9a541fa11c15eb676a41464f
ODC-ETHIOPIA <eyobmuktar4@gmail.com>
2021-04-21 12:17:27 +0300

    removed bootstrap alert

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,15 +1,12 @@
 import React from 'react'
 
-import Alert from 'react-bootstrap/Alert'
-
 export const Balance = ({balance}) => {
+  console.log(balance);
   const variant = balance > 0 ? 'success' : 'danger'
   return (
     <>
-    <Alert variant={variant} style={{height: '150px'}}>
-      <Alert.Heading variant="secondary" className="mt-4 ml-5 mb-2">My Balance</Alert.Heading>
+      <h3 className="mt-4 ml-5 mb-2">My Balance</h3>
       <h5 className="ml-5 font-weight-bold">{balance} Birr</h5>
-    </Alert>
     <hr />
     </>
   ) 
