5db214b1c5a744b0ff4a1b42632d82ac901a6eb7
ODC-ETHIOPIA <eyobmuktar4@gmail.com>
2021-04-21 13:32:49 +0300

    removed unused variable

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 export const Balance = ({balance}) => {
-  console.log(balance);
-  const variant = balance > 0 ? 'success' : 'danger'
   return (
     <>
       <h3 className="mt-4 ml-5 mb-2">My Balance</h3>
